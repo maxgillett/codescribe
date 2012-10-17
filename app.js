1db27a3c3de3be4faf69368d8bb192dc40fbb39a
Max Gillett <max.gillett@gmail.com>
2012-10-17 04:04:17 -0500

    Crude design layout

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var express = require('express');
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+  , jade = require('jade');
+
 var app = express();
+	app.set('view engine', 'jade');
+	app.set('views', "server/views");
+
+app.configure(function() {
+  function compile(str, path) {
+    return stylus(str)
+      .use(nib())
+      .import('nib')
+      .import(__dirname + '/server/views/reset');
+  }
+  this.use(stylus.middleware({
+    src: __dirname + "/server/views",
+    compile: compile,
+    force: true
+  }));
+  this.use(express.static(__dirname+'/server/views'));
+});
 
 app.get('/', function(req, res){
-	res.send('Hellow World');
+	res.render('index');
 });
 
 app.listen(3000);
